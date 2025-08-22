853be6ee184bc6ed706eefc60212d1325e632995
its-steo <steomustadd@gmail.com>
2025-08-22 15:19:26 +0300

    add new chnages

--- backend/gamblegalaxy/gamblegalaxy/settings.py
+++ backend/gamblegalaxy/gamblegalaxy/settings.py
@@ -126,7 +126,7 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sospetersamy@gmail.com'
 EMAIL_HOST_PASSWORD = 'yflegxikiihihdcl'  # App-specific password for Gmail
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'Gamble Galaxy <sospetersamy@gmail.com>'  # Must match EMAIL_HOST_USER or a verified alias
+DEFAULT_FROM_EMAIL = 'Gamble Galaxy <globalgrowthinvest@gmail.com>'  # Must match EMAIL_HOST_USER or a verified alias
 ADMIN_EMAIL = 'steomustadd@gmail.com'  # Admin email for deposit notifications
 
 import os
