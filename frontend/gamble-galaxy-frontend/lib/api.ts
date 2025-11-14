8d2072889dd11a4f8c58568b61449c49e2bbfe8c
its-steo <steomustadd@gmail.com>
2025-11-14 23:44:16 +0300

    add chnages

--- frontend/gamble-galaxy-frontend/lib/api.ts
+++ frontend/gamble-galaxy-frontend/lib/api.ts
@@ -27,7 +27,7 @@ interface BigGameImage {
   is_active?: boolean
 }
 
-//const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api"
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api"
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://gamblegalaxy.onrender.com/api"
 
 interface ApiResponse<T> {
